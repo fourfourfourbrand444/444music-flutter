6a2b3a2afd96373d69b4a925af40ff77077e03d0
OG Bluhface <fourfourfourbrand444@gmail.com>
2026-07-30 12:00:51 -0700

    Fix missing imports in MainActivity after package move

--- android/app/src/main/kotlin/com/app444music/app/MainActivity.kt
+++ android/app/src/main/kotlin/com/app444music/app/MainActivity.kt
@@ -1,5 +1,11 @@
 package com.app444music.app
 
+import android.content.Intent
+import android.os.Bundle
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
+
 class MainActivity : FlutterActivity() {
     private val CHANNEL = "com.app444music.app/deeplink"
 
